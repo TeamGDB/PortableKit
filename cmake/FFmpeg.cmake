NextOs-Ports/Yakumo

Keep the build machine's paths out of the bundled FFmpeg
FFmpeg stores its configure line, prefix included, as a string in
libavcodec and libavutil. The prefix was the build directory, so every
shipped copy named the home directory, and with it the user name, of the
machine that built it.

FFmpeg is now configured for the prefix /ffmpeg and installed through
DESTDIR into the same directory as before, so the include path the host
compiles against does not change. The stamp includes the prefix, which
rebuilds FFmpeg once in existing build directories.

Co-Authored-By: Claude Opus 5.5 (1M context) <noreply@anthropic.com>

profiles/mhp3rd/cmake/FFmpeg.cmake
@@ -195,6 +195,11 @@ else()
     endif()
     set(_mhp3rd_ffmpeg_src "${_mhp3rd_ffmpeg_root}/ffmpeg-${MHP3RD_FFMPEG_VERSION}")
     set(_mhp3rd_ffmpeg_prefix "${_mhp3rd_ffmpeg_root}/install")
+    # FFmpeg keeps its configure line, prefix included, as a string in the
+    # libraries. It is configured for a neutral prefix and installed through
+    # DESTDIR, so no path of the build machine (a home directory, a user name)
+    # ends up in a shipped library.
+    set(_mhp3rd_ffmpeg_neutral_prefix /ffmpeg)
     # Platform settings that do not change what is built: where macOS finds the
     # libraries (the executable's rpath) and the compiler to use.
     set(_mhp3rd_ffmpeg_flags ${MHP3RD_FFMPEG_CONFIGURE_FLAGS})
@@ -211,7 +216,7 @@ else()
     endif()
     # Built once per build directory; again only when the pin or the flags change.
     string(REPLACE ";" " " _mhp3rd_ffmpeg_stamp
-        "${MHP3RD_FFMPEG_VERSION} ${MHP3RD_FFMPEG_SHA256} ${_mhp3rd_ffmpeg_flags}")
+        "${MHP3RD_FFMPEG_VERSION} ${MHP3RD_FFMPEG_SHA256} --prefix=${_mhp3rd_ffmpeg_neutral_prefix} ${_mhp3rd_ffmpeg_flags}")
     set(_mhp3rd_ffmpeg_stamp_file "${_mhp3rd_ffmpeg_prefix}/yakumo.stamp")
     set(_mhp3rd_ffmpeg_built "")
     if(EXISTS "${_mhp3rd_ffmpeg_stamp_file}")
@@ -227,7 +232,7 @@ else()
         file(MAKE_DIRECTORY "${_mhp3rd_ffmpeg_root}/build")
         set(_log "${_mhp3rd_ffmpeg_root}/build")
         _mhp3rd_ffmpeg_run(configure "${_log}/configure.log"
-            sh "${_mhp3rd_ffmpeg_src}/configure" "--prefix=${_mhp3rd_ffmpeg_prefix}" ${_mhp3rd_ffmpeg_flags})
+            sh "${_mhp3rd_ffmpeg_src}/configure" "--prefix=${_mhp3rd_ffmpeg_neutral_prefix}" ${_mhp3rd_ffmpeg_flags})
         # The licensing in docs/SOURCE_PROVENANCE.md assumes exactly this.
         file(READ "${_log}/configure.log" _mhp3rd_ffmpeg_configure)
         file(READ "${_log}/config.h" _mhp3rd_ffmpeg_config)
@@ -238,7 +243,11 @@ else()
                 "see ${_log}/configure.log")
         endif()
         _mhp3rd_ffmpeg_run(build "${_log}/build.log" "${MHP3RD_MAKE}" -j${PSPRECOMP_GENERATED_JOBS})
-        _mhp3rd_ffmpeg_run(install "${_log}/install.log" "${MHP3RD_MAKE}" install)
+        file(REMOVE_RECURSE "${_mhp3rd_ffmpeg_root}/destdir")
+        _mhp3rd_ffmpeg_run(install "${_log}/install.log"
+            "${MHP3RD_MAKE}" install "DESTDIR=${_mhp3rd_ffmpeg_root}/destdir")
+        file(RENAME "${_mhp3rd_ffmpeg_root}/destdir${_mhp3rd_ffmpeg_neutral_prefix}" "${_mhp3rd_ffmpeg_prefix}")
+        file(REMOVE_RECURSE "${_mhp3rd_ffmpeg_root}/destdir")
         file(WRITE "${_mhp3rd_ffmpeg_stamp_file}" "${_mhp3rd_ffmpeg_stamp}")
     endif()
 

TeamGDB/PortableKit

Ported from Yakumo 3361439e4d.

cmake/FFmpeg.cmake
@@ -195,6 +195,11 @@ else()
     endif()
     set(_portablekit_ffmpeg_src "${_portablekit_ffmpeg_root}/ffmpeg-${PORTABLEKIT_FFMPEG_VERSION}")
     set(_portablekit_ffmpeg_prefix "${_portablekit_ffmpeg_root}/install")
+    # FFmpeg keeps its configure line, prefix included, as a string in the
+    # libraries. It is configured for a neutral prefix and installed through
+    # DESTDIR, so no path of the build machine (a home directory, a user name)
+    # ends up in a shipped library.
+    set(_portablekit_ffmpeg_neutral_prefix /ffmpeg)
     # Platform settings that do not change what is built: where macOS finds the
     # libraries (the executable's rpath) and the compiler to use.
     set(_portablekit_ffmpeg_flags ${PORTABLEKIT_FFMPEG_CONFIGURE_FLAGS})
@@ -228,7 +233,7 @@ else()
     endif()
     # Built once per build directory; again only when the pin or the flags change.
     string(REPLACE ";" " " _portablekit_ffmpeg_stamp
-        "${PORTABLEKIT_FFMPEG_VERSION} ${PORTABLEKIT_FFMPEG_SHA256} ${_portablekit_ffmpeg_flags}")
+        "${PORTABLEKIT_FFMPEG_VERSION} ${PORTABLEKIT_FFMPEG_SHA256} --prefix=${_portablekit_ffmpeg_neutral_prefix} ${_portablekit_ffmpeg_flags}")
     set(_portablekit_ffmpeg_stamp_file "${_portablekit_ffmpeg_prefix}/portablekit.stamp")
     set(_portablekit_ffmpeg_built "")
     if(EXISTS "${_portablekit_ffmpeg_stamp_file}")
@@ -244,7 +249,7 @@ else()
         file(MAKE_DIRECTORY "${_portablekit_ffmpeg_root}/build")
         set(_log "${_portablekit_ffmpeg_root}/build")
         _portablekit_ffmpeg_run(configure "${_log}/configure.log"
-            sh "${_portablekit_ffmpeg_src}/configure" "--prefix=${_portablekit_ffmpeg_prefix}" ${_portablekit_ffmpeg_flags})
+            sh "${_portablekit_ffmpeg_src}/configure" "--prefix=${_portablekit_ffmpeg_neutral_prefix}" ${_portablekit_ffmpeg_flags})
         # The licensing in docs/SOURCE_PROVENANCE.md assumes exactly this.
         file(READ "${_log}/configure.log" _portablekit_ffmpeg_configure)
         file(READ "${_log}/config.h" _portablekit_ffmpeg_config)
@@ -255,7 +260,11 @@ else()
                 "see ${_log}/configure.log")
         endif()
         _portablekit_ffmpeg_run(build "${_log}/build.log" "${PORTABLEKIT_MAKE}" -j${PSPRECOMP_GENERATED_JOBS})
-        _portablekit_ffmpeg_run(install "${_log}/install.log" "${PORTABLEKIT_MAKE}" install)
+        file(REMOVE_RECURSE "${_portablekit_ffmpeg_root}/destdir")
+        _portablekit_ffmpeg_run(install "${_log}/install.log"
+            "${PORTABLEKIT_MAKE}" install "DESTDIR=${_portablekit_ffmpeg_root}/destdir")
+        file(RENAME "${_portablekit_ffmpeg_root}/destdir${_portablekit_ffmpeg_neutral_prefix}" "${_portablekit_ffmpeg_prefix}")
+        file(REMOVE_RECURSE "${_portablekit_ffmpeg_root}/destdir")
         file(WRITE "${_portablekit_ffmpeg_stamp_file}" "${_portablekit_ffmpeg_stamp}")
     endif()
 