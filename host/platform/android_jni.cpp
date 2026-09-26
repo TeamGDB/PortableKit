NextOs-Ports/Yakumo

Show the whole picture on Android: full screen, clear of the cutout
The app ran as a normal window, and Android 15 draws apps under the status
and navigation bars, so the bars covered the top and bottom of the game:
the HUD's top line and the item box's name were cut. On Android the window
is now always full screen, with the system bars hidden.

The picture and the performance overlay go into the part of the window a
display cutout leaves free, and Fill takes that part's shape. SDL's safe
area also counts the gesture areas of the hidden bars, which would shrink
the picture for nothing, so the cutout comes from the app's own activity,
YakumoActivity (SDL's activity plus what the host needs from Android),
through a small JNI bridge (platform/android_jni). Everywhere else the
content area is the whole window, as before.

Co-Authored-By: Claude Opus 5.5 (1M context) <noreply@anthropic.com>

profiles/mhp3rd/host/platform/android_jni.cpp
@@ -1,0 +1,142 @@
+#include "android_jni.hpp"
+
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_system.h>
+
+#include <jni.h>
+
+namespace mhp3rd::android {
+namespace {
+
+// The activity's own class: a thread the JVM did not start cannot find app
+// classes by name, but the activity object knows its class.
+struct Call {
+    JNIEnv *env{};
+    jclass type{};
+    Call() {
+        env = static_cast<JNIEnv *>(SDL_GetAndroidJNIEnv());
+        if (env == nullptr) return;
+        auto activity = static_cast<jobject>(SDL_GetAndroidActivity());
+        if (activity == nullptr) return;
+        type = env->GetObjectClass(activity);
+        env->DeleteLocalRef(activity);
+    }
+    ~Call() {
+        if (env != nullptr && type != nullptr) env->DeleteLocalRef(type);
+    }
+    Call(const Call &) = delete;
+    Call &operator=(const Call &) = delete;
+    [[nodiscard]] bool ok() const { return env != nullptr && type != nullptr; }
+    jmethodID method(const char *name, const char *signature) const {
+        jmethodID id = env->GetStaticMethodID(type, name, signature);
+        if (id == nullptr) env->ExceptionClear();
+        return id;
+    }
+    // Clears a pending Java exception; true if there was one.
+    bool failed() const {
+        if (!env->ExceptionCheck()) return false;
+        env->ExceptionClear();
+        return true;
+    }
+    [[nodiscard]] std::optional<std::string> text(jobject object) const {
+        if (object == nullptr) return std::nullopt;
+        auto string = static_cast<jstring>(object);
+        const char *chars = env->GetStringUTFChars(string, nullptr);
+        std::optional<std::string> result;
+        if (chars != nullptr) result = std::string(chars);
+        env->ReleaseStringUTFChars(string, chars);
+        env->DeleteLocalRef(object);
+        return result;
+    }
+    jstring string(const std::string &value) const { return env->NewStringUTF(value.c_str()); }
+};
+
+} // namespace
+
+Insets cutout_insets() {
+    Insets insets;
+    Call call;
+    if (!call.ok()) return insets;
+    jmethodID id = call.method("cutoutInsets", "()[I");
+    if (id == nullptr) return insets;
+    auto array = static_cast<jintArray>(call.env->CallStaticObjectMethod(call.type, id));
+    if (call.failed() || array == nullptr) return insets;
+    jint values[4]{};
+    if (call.env->GetArrayLength(array) == 4) call.env->GetIntArrayRegion(array, 0, 4, values);
+    call.env->DeleteLocalRef(array);
+    return {values[0], values[1], values[2], values[3]};
+}
+
+std::optional<std::string> pick_folder() {
+    Call call;
+    if (!call.ok()) return std::nullopt;
+    jmethodID id = call.method("pickFolder", "()Ljava/lang/String;");
+    if (id == nullptr) return std::nullopt;
+    jobject result = call.env->CallStaticObjectMethod(call.type, id);
+    if (call.failed()) return std::nullopt;
+    return call.text(result);
+}
+
+std::string tree_root(const std::string &tree_uri) {
+    Call call;
+    if (!call.ok()) return {};
+    jmethodID id = call.method("treeRoot", "(Ljava/lang/String;)Ljava/lang/String;");
+    if (id == nullptr) return {};
+    jstring argument = call.string(tree_uri);
+    jobject result = call.env->CallStaticObjectMethod(call.type, id, argument);
+    call.env->DeleteLocalRef(argument);
+    if (call.failed()) return {};
+    return call.text(result).value_or(std::string());
+}
+
+std::optional<std::vector<Entry>> list_folder(const std::string &folder_uri) {
+    Call call;
+    if (!call.ok()) return std::nullopt;
+    jmethodID id = call.method("listFolder", "(Ljava/lang/String;)[Ljava/lang/String;");
+    if (id == nullptr) return std::nullopt;
+    jstring argument = call.string(folder_uri);
+    auto array = static_cast<jobjectArray>(call.env->CallStaticObjectMethod(call.type, id, argument));
+    call.env->DeleteLocalRef(argument);
+    if (call.failed() || array == nullptr) return std::nullopt;
+    std::vector<Entry> entries;
+    const jsize count = call.env->GetArrayLength(array);
+    for (jsize i = 0; i < count; ++i) {
+        const std::optional<std::string> line = call.text(call.env->GetObjectArrayElement(array, i));
+        if (!line || line->size() < 4u) continue;
+        const std::size_t name_end = line->find('/', 2u);
+        if (name_end == std::string::npos) continue;
+        entries.push_back({(*line)[0] == 'd', line->substr(2u, name_end - 2u), line->substr(name_end + 1u)});
+    }
+    call.env->DeleteLocalRef(array);
+    return entries;
+}
+
+std::optional<std::string> create(const std::string &folder_uri, const std::string &name, bool directory) {
+    Call call;
+    if (!call.ok()) return std::nullopt;
+    jmethodID id = call.method("create", "(Ljava/lang/String;Ljava/lang/String;Z)Ljava/lang/String;");
+    if (id == nullptr) return std::nullopt;
+    jstring folder = call.string(folder_uri);
+    jstring file = call.string(name);
+    jobject result = call.env->CallStaticObjectMethod(call.type, id, folder, file, directory ? JNI_TRUE : JNI_FALSE);
+    call.env->DeleteLocalRef(folder);
+    call.env->DeleteLocalRef(file);
+    if (call.failed()) return std::nullopt;
+    return call.text(result);
+}
+
+int open_document(const std::string &uri, const char *mode) {
+    Call call;
+    if (!call.ok()) return -1;
+    jmethodID id = call.method("openDocument", "(Ljava/lang/String;Ljava/lang/String;)I");
+    if (id == nullptr) return -1;
+    jstring document = call.string(uri);
+    jstring open_mode = call.env->NewStringUTF(mode);
+    const jint fd = call.env->CallStaticIntMethod(call.type, id, document, open_mode);
+    call.env->DeleteLocalRef(document);
+    call.env->DeleteLocalRef(open_mode);
+    if (call.failed()) return -1;
+    return fd;
+}
+
+} // namespace mhp3rd::android

profiles/mhp3rd/host/platform/android_jni.hpp
@@ -1,0 +1,43 @@
+#pragma once
+
+// What the Android app asks of its Java activity (YakumoActivity) through JNI:
+// the display cutout, and folders the player picks through the system's
+// document picker, read and written as content:// documents. Only in the
+// Android app (MHP3RD_ANDROID_APP); every call runs on the calling thread and
+// the picker blocks it until the player has chosen.
+
+#include <optional>
+#include <string>
+#include <vector>
+
+namespace mhp3rd::android {
+
+struct Insets {
+    int left{};
+    int top{};
+    int right{};
+    int bottom{};
+};
+// The display cutout, in window pixels; zero where there is none.
+[[nodiscard]] Insets cutout_insets();
+
+// A folder the player picks, as a tree URI; nothing when cancelled.
+[[nodiscard]] std::optional<std::string> pick_folder();
+// The document standing for a picked tree's folder itself.
+[[nodiscard]] std::string tree_root(const std::string &tree_uri);
+
+struct Entry {
+    bool directory{};
+    std::string name;
+    std::string uri;
+};
+// The children of a folder document; nothing if it cannot be read.
+[[nodiscard]] std::optional<std::vector<Entry>> list_folder(const std::string &folder_uri);
+// A new folder or file in a folder document, its URI; nothing on failure.
+[[nodiscard]] std::optional<std::string> create(const std::string &folder_uri, const std::string &name,
+                                                bool directory);
+// A file descriptor the caller closes, for reading ("r") or writing ("w",
+// truncating); -1 on failure.
+[[nodiscard]] int open_document(const std::string &uri, const char *mode);
+
+} // namespace mhp3rd::android

TeamGDB/PortableKit

Ported from Yakumo de05b60fba.

host/platform/android_jni.cpp
@@ -1,0 +1,142 @@
+#include "android_jni.hpp"
+
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_system.h>
+
+#include <jni.h>
+
+namespace portablekit::android {
+namespace {
+
+// The activity's own class: a thread the JVM did not start cannot find app
+// classes by name, but the activity object knows its class.
+struct Call {
+    JNIEnv *env{};
+    jclass type{};
+    Call() {
+        env = static_cast<JNIEnv *>(SDL_GetAndroidJNIEnv());
+        if (env == nullptr) return;
+        auto activity = static_cast<jobject>(SDL_GetAndroidActivity());
+        if (activity == nullptr) return;
+        type = env->GetObjectClass(activity);
+        env->DeleteLocalRef(activity);
+    }
+    ~Call() {
+        if (env != nullptr && type != nullptr) env->DeleteLocalRef(type);
+    }
+    Call(const Call &) = delete;
+    Call &operator=(const Call &) = delete;
+    [[nodiscard]] bool ok() const { return env != nullptr && type != nullptr; }
+    jmethodID method(const char *name, const char *signature) const {
+        jmethodID id = env->GetStaticMethodID(type, name, signature);
+        if (id == nullptr) env->ExceptionClear();
+        return id;
+    }
+    // Clears a pending Java exception; true if there was one.
+    bool failed() const {
+        if (!env->ExceptionCheck()) return false;
+        env->ExceptionClear();
+        return true;
+    }
+    [[nodiscard]] std::optional<std::string> text(jobject object) const {
+        if (object == nullptr) return std::nullopt;
+        auto string = static_cast<jstring>(object);
+        const char *chars = env->GetStringUTFChars(string, nullptr);
+        std::optional<std::string> result;
+        if (chars != nullptr) result = std::string(chars);
+        env->ReleaseStringUTFChars(string, chars);
+        env->DeleteLocalRef(object);
+        return result;
+    }
+    jstring string(const std::string &value) const { return env->NewStringUTF(value.c_str()); }
+};
+
+} // namespace
+
+Insets cutout_insets() {
+    Insets insets;
+    Call call;
+    if (!call.ok()) return insets;
+    jmethodID id = call.method("cutoutInsets", "()[I");
+    if (id == nullptr) return insets;
+    auto array = static_cast<jintArray>(call.env->CallStaticObjectMethod(call.type, id));
+    if (call.failed() || array == nullptr) return insets;
+    jint values[4]{};
+    if (call.env->GetArrayLength(array) == 4) call.env->GetIntArrayRegion(array, 0, 4, values);
+    call.env->DeleteLocalRef(array);
+    return {values[0], values[1], values[2], values[3]};
+}
+
+std::optional<std::string> pick_folder() {
+    Call call;
+    if (!call.ok()) return std::nullopt;
+    jmethodID id = call.method("pickFolder", "()Ljava/lang/String;");
+    if (id == nullptr) return std::nullopt;
+    jobject result = call.env->CallStaticObjectMethod(call.type, id);
+    if (call.failed()) return std::nullopt;
+    return call.text(result);
+}
+
+std::string tree_root(const std::string &tree_uri) {
+    Call call;
+    if (!call.ok()) return {};
+    jmethodID id = call.method("treeRoot", "(Ljava/lang/String;)Ljava/lang/String;");
+    if (id == nullptr) return {};
+    jstring argument = call.string(tree_uri);
+    jobject result = call.env->CallStaticObjectMethod(call.type, id, argument);
+    call.env->DeleteLocalRef(argument);
+    if (call.failed()) return {};
+    return call.text(result).value_or(std::string());
+}
+
+std::optional<std::vector<Entry>> list_folder(const std::string &folder_uri) {
+    Call call;
+    if (!call.ok()) return std::nullopt;
+    jmethodID id = call.method("listFolder", "(Ljava/lang/String;)[Ljava/lang/String;");
+    if (id == nullptr) return std::nullopt;
+    jstring argument = call.string(folder_uri);
+    auto array = static_cast<jobjectArray>(call.env->CallStaticObjectMethod(call.type, id, argument));
+    call.env->DeleteLocalRef(argument);
+    if (call.failed() || array == nullptr) return std::nullopt;
+    std::vector<Entry> entries;
+    const jsize count = call.env->GetArrayLength(array);
+    for (jsize i = 0; i < count; ++i) {
+        const std::optional<std::string> line = call.text(call.env->GetObjectArrayElement(array, i));
+        if (!line || line->size() < 4u) continue;
+        const std::size_t name_end = line->find('/', 2u);
+        if (name_end == std::string::npos) continue;
+        entries.push_back({(*line)[0] == 'd', line->substr(2u, name_end - 2u), line->substr(name_end + 1u)});
+    }
+    call.env->DeleteLocalRef(array);
+    return entries;
+}
+
+std::optional<std::string> create(const std::string &folder_uri, const std::string &name, bool directory) {
+    Call call;
+    if (!call.ok()) return std::nullopt;
+    jmethodID id = call.method("create", "(Ljava/lang/String;Ljava/lang/String;Z)Ljava/lang/String;");
+    if (id == nullptr) return std::nullopt;
+    jstring folder = call.string(folder_uri);
+    jstring file = call.string(name);
+    jobject result = call.env->CallStaticObjectMethod(call.type, id, folder, file, directory ? JNI_TRUE : JNI_FALSE);
+    call.env->DeleteLocalRef(folder);
+    call.env->DeleteLocalRef(file);
+    if (call.failed()) return std::nullopt;
+    return call.text(result);
+}
+
+int open_document(const std::string &uri, const char *mode) {
+    Call call;
+    if (!call.ok()) return -1;
+    jmethodID id = call.method("openDocument", "(Ljava/lang/String;Ljava/lang/String;)I");
+    if (id == nullptr) return -1;
+    jstring document = call.string(uri);
+    jstring open_mode = call.env->NewStringUTF(mode);
+    const jint fd = call.env->CallStaticIntMethod(call.type, id, document, open_mode);
+    call.env->DeleteLocalRef(document);
+    call.env->DeleteLocalRef(open_mode);
+    if (call.failed()) return -1;
+    return fd;
+}
+
+} // namespace portablekit::android

host/platform/android_jni.hpp
@@ -1,0 +1,43 @@
+#pragma once
+
+// What the Android app asks of its Java activity (YakumoActivity) through JNI:
+// the display cutout, and folders the player picks through the system's
+// document picker, read and written as content:// documents. Only in the
+// Android app (PORTABLEKIT_ANDROID_APP); every call runs on the calling thread and
+// the picker blocks it until the player has chosen.
+
+#include <optional>
+#include <string>
+#include <vector>
+
+namespace portablekit::android {
+
+struct Insets {
+    int left{};
+    int top{};
+    int right{};
+    int bottom{};
+};
+// The display cutout, in window pixels; zero where there is none.
+[[nodiscard]] Insets cutout_insets();
+
+// A folder the player picks, as a tree URI; nothing when cancelled.
+[[nodiscard]] std::optional<std::string> pick_folder();
+// The document standing for a picked tree's folder itself.
+[[nodiscard]] std::string tree_root(const std::string &tree_uri);
+
+struct Entry {
+    bool directory{};
+    std::string name;
+    std::string uri;
+};
+// The children of a folder document; nothing if it cannot be read.
+[[nodiscard]] std::optional<std::vector<Entry>> list_folder(const std::string &folder_uri);
+// A new folder or file in a folder document, its URI; nothing on failure.
+[[nodiscard]] std::optional<std::string> create(const std::string &folder_uri, const std::string &name,
+                                                bool directory);
+// A file descriptor the caller closes, for reading ("r") or writing ("w",
+// truncating); -1 on failure.
+[[nodiscard]] int open_document(const std::string &uri, const char *mode);
+
+} // namespace portablekit::android