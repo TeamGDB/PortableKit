NextOs-Ports/Yakumo

Cross-compile the bundled FFmpeg for Android
With the NDK toolchain the bundled FFmpeg configured for the build machine
and could not link into an arm64 Android executable. Under ANDROID it is now
configured as a cross build for aarch64 with the NDK's clang at the build's
API level and 16 KiB-aligned segments. FFmpeg's android target names the
libraries without a version (libavcodec.so, libavutil.so), which is also the
only form an APK's lib/ directory takes, so the import targets use those
names. Other platforms configure exactly as before.

Co-Authored-By: Claude Opus 5.5 (1M context) <noreply@anthropic.com>

profiles/mhp3rd/cmake/FFmpeg.cmake
@@ -206,7 +206,24 @@ else()
                 "--extra-ldflags=-mmacosx-version-min=${CMAKE_OSX_DEPLOYMENT_TARGET}")
         endif()
     endif()
-    if(DEFINED ENV{CC})
+    if(ANDROID)
+        # Cross-compiled with the NDK's clang for the ABI and API level of the
+        # rest of the build. FFmpeg's android target names the libraries
+        # libavcodec.so and libavutil.so, without a version, which is also the
+        # only form an APK's lib/ directory accepts.
+        if(NOT ANDROID_ABI STREQUAL "arm64-v8a")
+            message(FATAL_ERROR "mhp3rd: the bundled FFmpeg is set up for arm64-v8a only, not ${ANDROID_ABI}")
+        endif()
+        set(_mhp3rd_ndk_bin "${ANDROID_TOOLCHAIN_ROOT}/bin")
+        list(APPEND _mhp3rd_ffmpeg_flags
+            --enable-cross-compile --target-os=android --arch=aarch64
+            "--cc=${_mhp3rd_ndk_bin}/aarch64-linux-android${ANDROID_PLATFORM_LEVEL}-clang"
+            "--cxx=${_mhp3rd_ndk_bin}/aarch64-linux-android${ANDROID_PLATFORM_LEVEL}-clang++"
+            "--ar=${_mhp3rd_ndk_bin}/llvm-ar" "--nm=${_mhp3rd_ndk_bin}/llvm-nm"
+            "--ranlib=${_mhp3rd_ndk_bin}/llvm-ranlib" "--strip=${_mhp3rd_ndk_bin}/llvm-strip"
+            # Pages may be 16 KiB on current devices; the NDK's own flag for it.
+            "--extra-ldflags=-Wl,-z,max-page-size=16384")
+    elseif(DEFINED ENV{CC})
         list(APPEND _mhp3rd_ffmpeg_flags "--cc=$ENV{CC}")
     endif()
     # Built once per build directory; again only when the pin or the flags change.
@@ -251,6 +268,8 @@ else()
         list(POP_FRONT _versions lib soversion)
         if(APPLE)
             set(runtime_name "lib${lib}.${soversion}.dylib")
+        elseif(ANDROID)
+            set(runtime_name "lib${lib}.so")
         else()
             set(runtime_name "lib${lib}.so.${soversion}")
         endif()

TeamGDB/PortableKit

Ported from Yakumo c4bbf6c79d.

cmake/FFmpeg.cmake
@@ -206,7 +206,24 @@ else()
                 "--extra-ldflags=-mmacosx-version-min=${CMAKE_OSX_DEPLOYMENT_TARGET}")
         endif()
     endif()
-    if(DEFINED ENV{CC})
+    if(ANDROID)
+        # Cross-compiled with the NDK's clang for the ABI and API level of the
+        # rest of the build. FFmpeg's android target names the libraries
+        # libavcodec.so and libavutil.so, without a version, which is also the
+        # only form an APK's lib/ directory accepts.
+        if(NOT ANDROID_ABI STREQUAL "arm64-v8a")
+            message(FATAL_ERROR "portablekit: the bundled FFmpeg is set up for arm64-v8a only, not ${ANDROID_ABI}")
+        endif()
+        set(_portablekit_ndk_bin "${ANDROID_TOOLCHAIN_ROOT}/bin")
+        list(APPEND _portablekit_ffmpeg_flags
+            --enable-cross-compile --target-os=android --arch=aarch64
+            "--cc=${_portablekit_ndk_bin}/aarch64-linux-android${ANDROID_PLATFORM_LEVEL}-clang"
+            "--cxx=${_portablekit_ndk_bin}/aarch64-linux-android${ANDROID_PLATFORM_LEVEL}-clang++"
+            "--ar=${_portablekit_ndk_bin}/llvm-ar" "--nm=${_portablekit_ndk_bin}/llvm-nm"
+            "--ranlib=${_portablekit_ndk_bin}/llvm-ranlib" "--strip=${_portablekit_ndk_bin}/llvm-strip"
+            # Pages may be 16 KiB on current devices; the NDK's own flag for it.
+            "--extra-ldflags=-Wl,-z,max-page-size=16384")
+    elseif(DEFINED ENV{CC})
         list(APPEND _portablekit_ffmpeg_flags "--cc=$ENV{CC}")
     endif()
     # Built once per build directory; again only when the pin or the flags change.
@@ -251,6 +268,8 @@ else()
         list(POP_FRONT _versions lib soversion)
         if(APPLE)
             set(runtime_name "lib${lib}.${soversion}.dylib")
+        elseif(ANDROID)
+            set(runtime_name "lib${lib}.so")
         else()
             set(runtime_name "lib${lib}.so.${soversion}")
         endif()