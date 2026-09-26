NextOs-Ports/Yakumo

Set up from a disc image picked in Android's file picker
For #17. The first start on a phone runs without adb: "Choose disc image"
opens the system's document picker, and the picked .iso, a content://
document readable only through a file descriptor, is copied into the app's
storage with progress (and a cancel) before it is checked and the game
prepared from it, as on the desktop. Free space is checked first; the
setup says the copy needs about 1.3 GB besides the app's 0.8 GB, and that
the image cannot be used where it is.

An app cannot exec itself, so "Set up game data again" leaves a marker for
the next start and Android starts the app afresh (YakumoActivity.relaunch),
and "Restart now" after an import does the same. The finished screen names
the ways into the menu on a phone.

Tested on the API 35 emulator: an .iso in Download picked, copied, checked
and prepared; the game started; Set up game data again reopened the setup.

Co-Authored-By: Claude Opus 5.5 (1M context) <noreply@anthropic.com>

profiles/mhp3rd/host/install/installer.cpp
@@ -7,6 +7,9 @@
 #include "install/user_data.hpp"
 
 #include "kernel/iso_image.hpp"
+#if defined(MHP3RD_ANDROID_APP)
+#include "platform/android_jni.hpp"
+#endif
 
 #include "psprecomp/sha256.hpp"
 
@@ -316,6 +319,13 @@ bool restart_requested_on_exit() { return restart_on_exit; }
 int restart(char **argv) {
     std::cout.flush();
     std::cerr.flush();
+#if defined(MHP3RD_ANDROID_APP)
+    // An app cannot exec itself; Android starts it again instead.
+    (void)argv;
+    android::relaunch();
+    std::cerr << "Cannot restart; start Yakumo again to load the imported save\n";
+    return 1;
+#endif
     const std::filesystem::path self = executable_path();
     const std::string program = self.empty() ? std::string(argv[0]) : self.string();
 #if defined(_WIN32)
@@ -335,6 +345,15 @@ int restart_for_setup(const char *program) {
     // Nothing buffered survives the exec.
     std::cout.flush();
     std::cerr.flush();
+#if defined(MHP3RD_ANDROID_APP)
+    // The next start runs the setup: a marker in the data directory stands
+    // for --install, which an app is not started with.
+    (void)program;
+    { std::ofstream(user_data_directory() / kSetupMarkerFile) << "setup\n"; }
+    android::relaunch();
+    std::cerr << "Start Yakumo again to set up\n";
+    return 1;
+#endif
     // The player asked for the setup: a game directory chosen for this run
     // would make --install skip it.
 #if defined(_WIN32)

profiles/mhp3rd/host/main.cpp
@@ -339,6 +339,15 @@ int main(int argc, char **argv) {
             return 2;
         }
         if (options.install_image) return install_from_command_line(options);
+#if defined(MHP3RD_ANDROID_APP)
+        {
+            // "Set up game data again" from the menu, left for this start.
+            std::error_code ec;
+            const std::filesystem::path marker =
+                mhp3rd::install::user_data_directory() / mhp3rd::install::kSetupMarkerFile;
+            if (std::filesystem::remove(marker, ec)) options.install = true;
+        }
+#endif
 
         const std::optional<GameFiles> files = locate_game(options);
         if (!files) return 1;

TeamGDB/PortableKit

Ported from Yakumo fd57673f9c.

host/install/installer.cpp
@@ -7,6 +7,9 @@
 #include "install/user_data.hpp"
 
 #include "kernel/iso_image.hpp"
+#if defined(PORTABLEKIT_ANDROID_APP)
+#include "platform/android_jni.hpp"
+#endif
 
 #include "psprecomp/sha256.hpp"
 
@@ -320,6 +323,13 @@ bool restart_requested_on_exit() { return restart_on_exit; }
 int restart(char **argv) {
     std::cout.flush();
     std::cerr.flush();
+#if defined(PORTABLEKIT_ANDROID_APP)
+    // An app cannot exec itself; Android starts it again instead.
+    (void)argv;
+    android::relaunch();
+    std::cerr << "Cannot restart; start Yakumo again to load the imported save\n";
+    return 1;
+#endif
     const std::filesystem::path self = executable_path();
     const std::string program = self.empty() ? std::string(argv[0]) : self.string();
 #if defined(_WIN32)
@@ -339,6 +349,15 @@ int restart_for_setup(const char *program) {
     // Nothing buffered survives the exec.
     std::cout.flush();
     std::cerr.flush();
+#if defined(PORTABLEKIT_ANDROID_APP)
+    // The next start runs the setup: a marker in the data directory stands
+    // for --install, which an app is not started with.
+    (void)program;
+    { std::ofstream(user_data_directory() / kSetupMarkerFile) << "setup\n"; }
+    android::relaunch();
+    std::cerr << "Start Yakumo again to set up\n";
+    return 1;
+#endif
     // The player asked for the setup: a game directory chosen for this run
     // would make --install skip it.
 #if defined(_WIN32)

host/main.cpp
@@ -336,6 +336,15 @@ int main(int argc, char **argv) {
             return 2;
         }
         if (options.install_image) return install_from_command_line(options);
+#if defined(PORTABLEKIT_ANDROID_APP)
+        {
+            // "Set up game data again" from the menu, left for this start.
+            std::error_code ec;
+            const std::filesystem::path marker =
+                portablekit::install::user_data_directory() / portablekit::install::kSetupMarkerFile;
+            if (std::filesystem::remove(marker, ec)) options.install = true;
+        }
+#endif
 
         const std::optional<GameFiles> files = locate_game(options);
         if (!files) return 1;