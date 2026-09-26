NextOs-Ports/Yakumo

Build the host as an Android app's libmain.so
MHP3RD_ANDROID_APP builds Yakumo as libmain.so for SDL's Java activity
instead of an executable, for issue #17's research APK:

- android_app.cpp is the SDL_main entry point. SDL calls it on a Java
  thread with a small stack, and an app cannot restart itself with a larger
  one as the Linux executable does, so it runs the game on a thread with the
  64 MiB stack the other platforms give their main thread, and sends stdout
  and stderr to yakumo.log in the app's internal storage.
- The host's code is libmain.so inside app_process, so executable_path()
  names the library through dladdr, and the overlays are looked for next to
  it, in the flat native library directory the package manager extracted.
- The package manager extracts only lib*.so, so the overlays are named
  libovl*.so, and only those are loaded from that directory.
- The app loads libmain.so into its own linker namespace, not the global
  one, so the overlays link against it and find the host's symbols as a
  dependency instead of from the executable.

Without the option nothing changes, on Android or elsewhere. Tested on the
API 35 arm64 emulator: the app finds all 355 overlays, starts the renderer
on SwiftShader, plays the intro movie and reaches character creation.

Co-Authored-By: Claude Opus 5.5 (1M context) <noreply@anthropic.com>

profiles/mhp3rd/host/app_paths.cpp
@@ -9,6 +9,8 @@
 #include <windows.h>
 #elif defined(__APPLE__)
 #include <mach-o/dyld.h>
+#elif defined(MHP3RD_ANDROID_APP)
+#include <dlfcn.h>
 #endif
 
 namespace mhp3rd {
@@ -28,6 +30,12 @@ std::filesystem::path executable_path() {
     std::string buffer(size, '\0');
     if (_NSGetExecutablePath(buffer.data(), &size) != 0) return {};
     return std::filesystem::canonical(buffer.c_str());
+#elif defined(MHP3RD_ANDROID_APP)
+    // An Android app runs inside app_process; its own code is libmain.so, in
+    // the directory the package manager extracted the APK's libraries to.
+    Dl_info info{};
+    if (dladdr(reinterpret_cast<const void *>(&executable_path), &info) == 0 || info.dli_fname == nullptr) return {};
+    return std::filesystem::path(info.dli_fname);
 #else
     std::error_code ec;
     const std::filesystem::path self = std::filesystem::read_symlink("/proc/self/exe", ec);

profiles/mhp3rd/host/main.cpp
@@ -59,7 +59,7 @@ constexpr EmbeddedNid kEmbeddedNids[] = {
 #include "nid_table.inc"
 };
 
-#if defined(__linux__)
+#if defined(__linux__) && !defined(MHP3RD_ANDROID_APP)
 // Chained AOT calls nest native frames deeply and need the 64 MiB stack the
 // other platforms get from their linker. ELF has no such option: the main
 // thread's stack is sized by RLIMIT_STACK when the program starts, so raise the
@@ -314,8 +314,15 @@ int run_adhoc_server(int argc, char **argv) {
 
 } // namespace
 
+#if defined(MHP3RD_ANDROID_APP)
+// android_app.cpp owns the entry point SDL calls and runs this on a thread
+// with the stack the game needs; restarting the process to get one is not an
+// option inside an app.
+int yakumo_main(int argc, char **argv) {
+#else
 int main(int argc, char **argv) {
-#if defined(__linux__)
+#endif
+#if defined(__linux__) && !defined(MHP3RD_ANDROID_APP)
     ensure_main_stack(argv);
 #endif
     try {

profiles/mhp3rd/host/overlays.cpp
@@ -129,13 +129,24 @@ std::map<std::uint32_t, std::uint64_t> &installed_headers() {
 std::filesystem::path overlay_directory() {
     if (const char *dir = std::getenv("MHP3RD_OVERLAY_DIR"); dir != nullptr && *dir != '\0') return dir;
     const std::filesystem::path directory = executable_directory();
+#if defined(MHP3RD_ANDROID_APP)
+    // An APK's libraries all sit in one flat directory, next to libmain.so.
+    return directory;
+#else
     if (directory.empty()) return "overlays";
     return directory / "overlays";
+#endif
 }
 
 bool is_overlay_library(const std::filesystem::path &path) {
     const std::filesystem::path extension = path.extension();
+#if defined(MHP3RD_ANDROID_APP)
+    // The package manager extracts only lib*.so, so the overlays are named
+    // libovl*.so there, among SDL's, FFmpeg's and the host's own.
+    return extension == ".so" && path.filename().string().starts_with("libovl");
+#else
     return extension == ".so" || extension == ".dylib" || extension == ".dll";
+#endif
 }
 
 // The library stays mapped for the rest of the process: its code can be reached

TeamGDB/PortableKit

Ported from Yakumo d8f19c8f42.

host/app_paths.cpp
@@ -9,6 +9,8 @@
 #include <windows.h>
 #elif defined(__APPLE__)
 #include <mach-o/dyld.h>
+#elif defined(PORTABLEKIT_ANDROID_APP)
+#include <dlfcn.h>
 #endif
 
 namespace portablekit {
@@ -28,6 +30,12 @@ std::filesystem::path executable_path() {
     std::string buffer(size, '\0');
     if (_NSGetExecutablePath(buffer.data(), &size) != 0) return {};
     return std::filesystem::canonical(buffer.c_str());
+#elif defined(PORTABLEKIT_ANDROID_APP)
+    // An Android app runs inside app_process; its own code is libmain.so, in
+    // the directory the package manager extracted the APK's libraries to.
+    Dl_info info{};
+    if (dladdr(reinterpret_cast<const void *>(&executable_path), &info) == 0 || info.dli_fname == nullptr) return {};
+    return std::filesystem::path(info.dli_fname);
 #else
     std::error_code ec;
     const std::filesystem::path self = std::filesystem::read_symlink("/proc/self/exe", ec);

host/main.cpp
@@ -51,7 +51,7 @@ constexpr EmbeddedNid kEmbeddedNids[] = {
 #include "nid_table.inc"
 };
 
-#if defined(__linux__)
+#if defined(__linux__) && !defined(PORTABLEKIT_ANDROID_APP)
 // Chained AOT calls nest native frames deeply and need the 64 MiB stack the
 // other platforms get from their linker. ELF has no such option: the main
 // thread's stack is sized by RLIMIT_STACK when the program starts, so raise the
@@ -311,8 +311,15 @@ int run_adhoc_server(int argc, char **argv) {
 
 } // namespace
 
+#if defined(PORTABLEKIT_ANDROID_APP)
+// android_app.cpp owns the entry point SDL calls and runs this on a thread
+// with the stack the game needs; restarting the process to get one is not an
+// option inside an app.
+int portablekit_main(int argc, char **argv) {
+#else
 int main(int argc, char **argv) {
-#if defined(__linux__)
+#endif
+#if defined(__linux__) && !defined(PORTABLEKIT_ANDROID_APP)
     ensure_main_stack(argv);
 #endif
     try {

host/overlays.cpp
@@ -131,13 +131,24 @@ std::map<std::uint32_t, std::uint64_t> &installed_headers() {
 std::filesystem::path overlay_directory() {
     if (const char *dir = portablekit::env("OVERLAY_DIR"); dir != nullptr && *dir != '\0') return dir;
     const std::filesystem::path directory = executable_directory();
+#if defined(PORTABLEKIT_ANDROID_APP)
+    // An APK's libraries all sit in one flat directory, next to libmain.so.
+    return directory;
+#else
     if (directory.empty()) return "overlays";
     return directory / "overlays";
+#endif
 }
 
 bool is_overlay_library(const std::filesystem::path &path) {
     const std::filesystem::path extension = path.extension();
+#if defined(PORTABLEKIT_ANDROID_APP)
+    // The package manager extracts only lib*.so, so the overlays are named
+    // libovl*.so there, among SDL's, FFmpeg's and the host's own.
+    return extension == ".so" && path.filename().string().starts_with("libovl");
+#else
     return extension == ".so" || extension == ".dylib" || extension == ".dll";
+#endif
 }
 
 // The library stays mapped for the rest of the process: its code can be reached