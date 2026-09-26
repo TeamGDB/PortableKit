NextOs-Ports/Yakumo

Copy a picked Android document in whichever setup asks for it
A player's setup stopped at '"content://com.android.externalstorage
.documents/document/primary%3ADownload%2F....iso" is not a file Yakumo
can open.' That happens when the setup screens cannot be shown and the
SDL dialogs stand in for them: SDL's file dialog on Android returns the
picked document's content:// URI, which went to the image check as if it
were a path. Only the setup screens copied the document in first.

- The copy of a picked document (copy_image_document) moves from the
  setup screens into the installer, and run_installer() copies any
  content:// image a front end returns before checking it.
- The SDL dialogs on Android pick with the app's own document picker
  (every file type, as the setup screens do), say that the copy takes a
  while, always copy, and no longer offer to use the image in place or
  mention the Steam Deck.
- A document another app opens with Yakumo reaches SDL only as the path
  part of its URI, which names no file; it is logged and ignored instead
  of being taken for a dropped file.
- The file browser (mods, texture packs) does not offer the system dialog
  on Android, where it gives documents nothing there can read, and those
  screens say that importing through Android's picker is not supported
  yet instead of offering to drop a folder.

Co-Authored-By: Claude Opus 5.5 (1M context) <noreply@anthropic.com>

profiles/mhp3rd/host/install/installer.cpp
@@ -9,6 +9,11 @@
 #include "kernel/iso_image.hpp"
 #if defined(MHP3RD_ANDROID_APP)
 #include "platform/android_jni.hpp"
+
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#include <cerrno>
 #endif
 
 #include "psprecomp/sha256.hpp"
@@ -262,15 +267,81 @@ void install(const std::filesystem::path &image, ImageStorage storage, const std
     }
 }
 
+#if defined(MHP3RD_ANDROID_APP)
+bool is_document_uri(const std::filesystem::path &image) { return path_to_utf8(image).rfind("content://", 0) == 0; }
+
+std::filesystem::path copy_image_document(const std::string &uri, const std::filesystem::path &data_dir,
+                                          const ProgressFn &progress) {
+    const int fd = android::open_document(uri, "r");
+    if (fd < 0) throw InstallError("Android would not let Yakumo read that file. Choose it again.");
+    struct stat info {};
+    const std::uint64_t size = ::fstat(fd, &info) == 0 ? static_cast<std::uint64_t>(info.st_size) : 0u;
+    // The copy, with room to spare for the executable prepared from it.
+    const std::uint64_t needed = size + kFreeSpaceMargin;
+    if (const auto space = available_space(data_dir); space && size != 0u && *space < needed) {
+        ::close(fd);
+        throw InstallError("Not enough free space to copy the disc image: it needs " + megabytes(needed) + " and " +
+                           megabytes(*space) + " is free on this device.");
+    }
+    std::error_code ec;
+    std::filesystem::create_directories(data_dir, ec);
+    const std::filesystem::path target = data_dir / kCopiedImageFile;
+    const std::filesystem::path partial = data_dir / (std::string(kCopiedImageFile) + ".part");
+    const std::string stage = "Copying the disc image";
+    try {
+        std::ofstream out(partial, std::ios::binary | std::ios::trunc);
+        std::vector<char> buffer(4u << 20);
+        std::uint64_t done = 0u;
+        progress(stage, 0u, size);
+        for (;;) {
+            const ssize_t got = ::read(fd, buffer.data(), buffer.size());
+            if (got < 0 && errno == EINTR) continue;
+            if (got < 0) throw InstallError("Reading the disc image failed. Choose it again.");
+            if (got == 0) break;
+            out.write(buffer.data(), got);
+            if (!out)
+                throw InstallError("Writing the copy of the disc image failed. Check that the device has free space.");
+            done += static_cast<std::uint64_t>(got);
+            progress(stage, done, size);
+        }
+        out.close();
+        if (!out)
+            throw InstallError("Writing the copy of the disc image failed. Check that the device has free space.");
+    } catch (...) {
+        ::close(fd);
+        std::filesystem::remove(partial, ec);
+        throw;
+    }
+    ::close(fd);
+    std::filesystem::rename(partial, target, ec);
+    if (ec) throw InstallError("Could not keep the copy of the disc image: " + ec.message() + ".");
+    return target;
+}
+#endif
+
 void InstallerUi::run_task(const std::string &, const std::function<void()> &work) { work(); }
 
 bool run_installer(InstallerUi &ui, const std::filesystem::path &data_dir) {
     for (;;) {
         if (!ui.introduce(data_dir)) return false;
         for (;;) {
-            const auto image = ui.choose_image();
+            auto image = ui.choose_image();
             if (!image) break;  // back to the introduction
             try {
+#if defined(MHP3RD_ANDROID_APP)
+                // A document that reached here as it is (the SDL dialogs'
+                // picker returns one) is copied in first, as the setup
+                // screens do before they return it.
+                if (is_document_uri(*image)) {
+                    const std::string uri = path_to_utf8(*image);
+                    std::cout << "[setup] copying " << uri << std::endl;
+                    ui.run_task("Copying the disc image", [&] {
+                        image = copy_image_document(uri, data_dir,
+                                                    [&ui](const std::string &stage, std::uint64_t done,
+                                                          std::uint64_t total) { ui.progress(stage, done, total); });
+                    });
+                }
+#endif
                 ImageInfo info;
                 ui.run_task("Checking the disc image", [&] { info = check_image(*image); });
                 const auto storage = ui.choose_storage(*image, info, data_dir);

profiles/mhp3rd/host/install/installer.hpp
@@ -60,6 +60,18 @@ using ProgressFn = std::function<void(const std::string &stage, std::uint64_t do
 void install(const std::filesystem::path &image, ImageStorage storage, const std::filesystem::path &data_dir,
              const ProgressFn &progress);
 
+#if defined(MHP3RD_ANDROID_APP)
+// Android: whether an image the player chose is a content:// document (from
+// the system's file picker or another app) rather than a file. A document can
+// only be read through a file descriptor, never opened by its name.
+[[nodiscard]] bool is_document_uri(const std::filesystem::path &image);
+// Copies such a document into data_dir as the copied image (kCopiedImageFile)
+// and returns where it is. Throws InstallError, or InstallCancelled from
+// progress; a partial copy is removed.
+std::filesystem::path copy_image_document(const std::string &uri, const std::filesystem::path &data_dir,
+                                          const ProgressFn &progress);
+#endif
+
 class InstallerUi {
 public:
     virtual ~InstallerUi() = default;

profiles/mhp3rd/host/ui/file_browser.cpp
@@ -241,8 +241,14 @@ FileBrowser::Result FileBrowser::frame(bool back) {
         open(folder_);
     }
     // Under gamescope the portal dialog never shows; do not offer it there.
-    const bool gamescope = std::getenv("GAMESCOPE_WAYLAND_DISPLAY") != nullptr;
-    if (!gamescope && chip("##system", "System dialog\u2026")) {
+    // On Android it gives content:// documents, which nothing that uses the
+    // browser can read by name.
+#if defined(MHP3RD_ANDROID_APP)
+    const bool system_dialog = false;
+#else
+    const bool system_dialog = std::getenv("GAMESCOPE_WAYLAND_DISPLAY") == nullptr;
+#endif
+    if (system_dialog && chip("##system", "System dialog\u2026")) {
         static const SDL_DialogFileFilter kFilters[] = {{"Disc images (*.iso)", "iso"}, {"All files", "*"}};
         const bool folders = !options_.choose_folder.empty();
         auto *state = new std::shared_ptr<SystemDialog>(dialog_);

profiles/mhp3rd/host/ui/texture_pack_screen.cpp
@@ -174,8 +174,16 @@ void log_review(const Review &r) {
 
 bool browse(bool back) {
     State &s = state();
+#if defined(MHP3RD_ANDROID_APP)
+    indented("Import: open the pack's folder, the one that holds textures.ini, or choose a folder that holds it, "
+             "such as PPSSPP's PSP/TEXTURES.");
+    indented("On Android this lists only folders Yakumo can read by itself, which leaves out Downloads and SD "
+             "cards. Importing through Android's file picker is not supported yet.",
+             colors::kTextDim);
+#else
     indented("Import: open the pack's folder, the one that holds textures.ini, or choose a folder that holds it, "
              "such as PPSSPP's PSP/TEXTURES. You can also drop the folder on the window.");
+#endif
     // A folder dropped on the window is chosen at once.
     if (auto dropped = Layer::get().take_dropped_file()) {
         s.last_folder = s.browser->folder();

TeamGDB/PortableKit

Ported from Yakumo 44514dd5f4.

host/install/installer.cpp
@@ -9,6 +9,11 @@
 #include "kernel/iso_image.hpp"
 #if defined(PORTABLEKIT_ANDROID_APP)
 #include "platform/android_jni.hpp"
+
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#include <cerrno>
 #endif
 
 #include "psprecomp/sha256.hpp"
@@ -266,15 +271,82 @@ void install(const std::filesystem::path &image, ImageStorage storage, const std
     }
 }
 
+#if defined(PORTABLEKIT_ANDROID_APP)
+bool is_document_uri(const std::filesystem::path &image) { return path_to_utf8(image).rfind("content://", 0) == 0; }
+
+std::filesystem::path copy_image_document(const std::string &uri, const std::filesystem::path &data_dir,
+                                          const ProgressFn &progress) {
+    const int fd = android::open_document(uri, "r");
+    if (fd < 0) throw InstallError(std::string("Android would not let ") + portablekit::game().project_name +
+                                     " read that file. Choose it again.");
+    struct stat info {};
+    const std::uint64_t size = ::fstat(fd, &info) == 0 ? static_cast<std::uint64_t>(info.st_size) : 0u;
+    // The copy, with room to spare for the executable prepared from it.
+    const std::uint64_t needed = size + kFreeSpaceMargin;
+    if (const auto space = available_space(data_dir); space && size != 0u && *space < needed) {
+        ::close(fd);
+        throw InstallError("Not enough free space to copy the disc image: it needs " + megabytes(needed) + " and " +
+                           megabytes(*space) + " is free on this device.");
+    }
+    std::error_code ec;
+    std::filesystem::create_directories(data_dir, ec);
+    const std::filesystem::path target = data_dir / kCopiedImageFile;
+    const std::filesystem::path partial = data_dir / (std::string(kCopiedImageFile) + ".part");
+    const std::string stage = "Copying the disc image";
+    try {
+        std::ofstream out(partial, std::ios::binary | std::ios::trunc);
+        std::vector<char> buffer(4u << 20);
+        std::uint64_t done = 0u;
+        progress(stage, 0u, size);
+        for (;;) {
+            const ssize_t got = ::read(fd, buffer.data(), buffer.size());
+            if (got < 0 && errno == EINTR) continue;
+            if (got < 0) throw InstallError("Reading the disc image failed. Choose it again.");
+            if (got == 0) break;
+            out.write(buffer.data(), got);
+            if (!out)
+                throw InstallError("Writing the copy of the disc image failed. Check that the device has free space.");
+            done += static_cast<std::uint64_t>(got);
+            progress(stage, done, size);
+        }
+        out.close();
+        if (!out)
+            throw InstallError("Writing the copy of the disc image failed. Check that the device has free space.");
+    } catch (...) {
+        ::close(fd);
+        std::filesystem::remove(partial, ec);
+        throw;
+    }
+    ::close(fd);
+    std::filesystem::rename(partial, target, ec);
+    if (ec) throw InstallError("Could not keep the copy of the disc image: " + ec.message() + ".");
+    return target;
+}
+#endif
+
 void InstallerUi::run_task(const std::string &, const std::function<void()> &work) { work(); }
 
 bool run_installer(InstallerUi &ui, const std::filesystem::path &data_dir) {
     for (;;) {
         if (!ui.introduce(data_dir)) return false;
         for (;;) {
-            const auto image = ui.choose_image();
+            auto image = ui.choose_image();
             if (!image) break;  // back to the introduction
             try {
+#if defined(PORTABLEKIT_ANDROID_APP)
+                // A document that reached here as it is (the SDL dialogs'
+                // picker returns one) is copied in first, as the setup
+                // screens do before they return it.
+                if (is_document_uri(*image)) {
+                    const std::string uri = path_to_utf8(*image);
+                    std::cout << "[setup] copying " << uri << std::endl;
+                    ui.run_task("Copying the disc image", [&] {
+                        image = copy_image_document(uri, data_dir,
+                                                    [&ui](const std::string &stage, std::uint64_t done,
+                                                          std::uint64_t total) { ui.progress(stage, done, total); });
+                    });
+                }
+#endif
                 ImageInfo info;
                 ui.run_task("Checking the disc image", [&] { info = check_image(*image); });
                 const auto storage = ui.choose_storage(*image, info, data_dir);

host/install/installer.hpp
@@ -60,6 +60,18 @@ using ProgressFn = std::function<void(const std::string &stage, std::uint64_t do
 void install(const std::filesystem::path &image, ImageStorage storage, const std::filesystem::path &data_dir,
              const ProgressFn &progress);
 
+#if defined(PORTABLEKIT_ANDROID_APP)
+// Android: whether an image the player chose is a content:// document (from
+// the system's file picker or another app) rather than a file. A document can
+// only be read through a file descriptor, never opened by its name.
+[[nodiscard]] bool is_document_uri(const std::filesystem::path &image);
+// Copies such a document into data_dir as the copied image (kCopiedImageFile)
+// and returns where it is. Throws InstallError, or InstallCancelled from
+// progress; a partial copy is removed.
+std::filesystem::path copy_image_document(const std::string &uri, const std::filesystem::path &data_dir,
+                                          const ProgressFn &progress);
+#endif
+
 class InstallerUi {
 public:
     virtual ~InstallerUi() = default;

host/ui/file_browser.cpp
@@ -241,8 +241,14 @@ FileBrowser::Result FileBrowser::frame(bool back) {
         open(folder_);
     }
     // Under gamescope the portal dialog never shows; do not offer it there.
-    const bool gamescope = std::getenv("GAMESCOPE_WAYLAND_DISPLAY") != nullptr;
-    if (!gamescope && chip("##system", "System dialog\u2026")) {
+    // On Android it gives content:// documents, which nothing that uses the
+    // browser can read by name.
+#if defined(PORTABLEKIT_ANDROID_APP)
+    const bool system_dialog = false;
+#else
+    const bool system_dialog = std::getenv("GAMESCOPE_WAYLAND_DISPLAY") == nullptr;
+#endif
+    if (system_dialog && chip("##system", "System dialog\u2026")) {
         static const SDL_DialogFileFilter kFilters[] = {{"Disc images (*.iso)", "iso"}, {"All files", "*"}};
         const bool folders = !options_.choose_folder.empty();
         auto *state = new std::shared_ptr<SystemDialog>(dialog_);

host/ui/texture_pack_screen.cpp
@@ -174,8 +174,16 @@ void log_review(const Review &r) {
 
 bool browse(bool back) {
     State &s = state();
+#if defined(PORTABLEKIT_ANDROID_APP)
+    indented("Import: open the pack's folder, the one that holds textures.ini, or choose a folder that holds it, "
+             "such as PPSSPP's PSP/TEXTURES.");
+    indented("On Android this lists only folders Yakumo can read by itself, which leaves out Downloads and SD "
+             "cards. Importing through Android's file picker is not supported yet.",
+             colors::kTextDim);
+#else
     indented("Import: open the pack's folder, the one that holds textures.ini, or choose a folder that holds it, "
              "such as PPSSPP's PSP/TEXTURES. You can also drop the folder on the window.");
+#endif
     // A folder dropped on the window is chosen at once.
     if (auto dropped = Layer::get().take_dropped_file()) {
         s.last_folder = s.browser->folder();