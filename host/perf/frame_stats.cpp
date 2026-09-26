NextOs-Ports/Yakumo

Tell Android how long each frame's work takes (performance hints)
The game's thread works in a burst each frame and then sleeps until
real time catches up, so the kernel sees a mostly idle thread and keeps
its core's clock low, and each frame's burst runs slowly. That is a
likely part of phones lagging whose CPU is fast enough on paper.
Android 13 added performance hints for this (ADPF): the app reports
the time a frame's work took against a target, and the system raises
the clock before frames run late.

The Android app now opens a hint session for the game's thread (the
functions are looked up at run time, as the app supports Android 11)
with a target of 25 ms of work a frame, three quarters of the PSP's
frame, and reports each game frame's time without the sleep that holds
the game to real time. The log says whether hints are on;
MHP3RD_NO_PERFORMANCE_HINT turns them off.

Co-Authored-By: Claude Opus 5.5 (1M context) <noreply@anthropic.com>

profiles/mhp3rd/host/perf/frame_stats.cpp
@@ -2,6 +2,10 @@
 
 #include "settings/settings.hpp"
 
+#if defined(MHP3RD_ANDROID_APP)
+#include "platform/android_performance.hpp"
+#endif
+
 #include <algorithm>
 #include <cstdio>
 #include <cstdlib>
@@ -78,6 +82,7 @@ const StallTrace &stall_trace() {
 struct State {
     // Frame in progress.
     Clock::time_point frame_start{Clock::now()};
+    Clock::time_point frame_start_previous{Clock::now()};  // of the frame end_frame() closes
     Clock::duration render{};
     Clock::duration wait{};
     Clock::duration pacing{};
@@ -323,6 +328,7 @@ void end_frame(std::uint64_t virtual_us, bool presented) {
     if (presented) count_present();
     const Clock::time_point now = Clock::now();
     const double frame_ms = to_ms(now - s.frame_start);
+    s.frame_start_previous = s.frame_start;
     s.frame_start = now;
 
     if (!s.window_has_clock) {
@@ -345,6 +351,11 @@ void end_frame(std::uint64_t virtual_us, bool presented) {
         ++s.gpu_frames;
     }
     ++s.frame_number;
+#if defined(MHP3RD_ANDROID_APP)
+    // The frame's work, without the sleep that holds the game to real time.
+    android::report_frame_work(
+        std::chrono::duration_cast<std::chrono::nanoseconds>(now - (s.frame_start_previous + s.pacing)).count());
+#endif
     const StallTrace &trace = stall_trace();
     if (trace.enabled && frame_ms > trace.slow_frame_ms) {
         // The GPU time is the previous frame's: that is the work a fence wait

profiles/mhp3rd/host/platform/android_performance.cpp
@@ -1,0 +1,65 @@
+#include "platform/android_performance.hpp"
+
+#include <dlfcn.h>
+#include <unistd.h>
+
+#include <cstdlib>
+#include <iostream>
+
+namespace mhp3rd::android {
+namespace {
+
+// The time a game frame's work should take: three quarters of the PSP's
+// 29.97 frames a second, so the system raises the clock while a frame still
+// has room rather than once it is late.
+constexpr std::int64_t kTargetWorkNs = 25'000'000;
+
+// Resolved at run time: the functions exist from API 33, and the app runs on
+// Android 11 and later.
+struct Hints {
+    using GetManager = void *(*)();
+    using CreateSession = void *(*)(void *, const std::int32_t *, std::size_t, std::int64_t);
+    using ReportActual = int (*)(void *, std::int64_t);
+    void *session{};
+    ReportActual report{};
+    bool opened{};
+};
+
+Hints &hints() {
+    static Hints value;
+    return value;
+}
+
+void open(Hints &hints) {
+    hints.opened = true;
+    if (std::getenv("MHP3RD_NO_PERFORMANCE_HINT") != nullptr) {
+        std::cout << "[perf] performance hints off (MHP3RD_NO_PERFORMANCE_HINT)\n";
+        return;
+    }
+    void *library = dlopen("libandroid.so", RTLD_NOW | RTLD_LOCAL);
+    if (library == nullptr) return;
+    const auto get_manager = reinterpret_cast<Hints::GetManager>(dlsym(library, "APerformanceHint_getManager"));
+    const auto create_session =
+        reinterpret_cast<Hints::CreateSession>(dlsym(library, "APerformanceHint_createSession"));
+    hints.report = reinterpret_cast<Hints::ReportActual>(dlsym(library, "APerformanceHint_reportActualWorkDuration"));
+    if (get_manager == nullptr || create_session == nullptr || hints.report == nullptr) {
+        std::cout << "[perf] no performance hints on this Android version\n";
+        return;
+    }
+    void *manager = get_manager();
+    const std::int32_t thread = static_cast<std::int32_t>(gettid());
+    hints.session = manager != nullptr ? create_session(manager, &thread, 1u, kTargetWorkNs) : nullptr;
+    std::cout << "[perf] performance hints " << (hints.session != nullptr ? "on" : "unavailable")
+              << " for the game's thread, target " << kTargetWorkNs / 1'000'000 << " ms of work a frame\n";
+}
+
+} // namespace
+
+void report_frame_work(std::int64_t work_ns) {
+    Hints &state = hints();
+    if (!state.opened) open(state);
+    if (state.session == nullptr || work_ns <= 0) return;
+    state.report(state.session, work_ns);
+}
+
+} // namespace mhp3rd::android

profiles/mhp3rd/host/platform/android_performance.hpp
@@ -1,0 +1,19 @@
+#pragma once
+
+#include <cstdint>
+
+// Android's performance hints (ADPF, APerformanceHint, Android 13 and later).
+// The game's thread works in a burst each frame and then sleeps until real
+// time catches up, so on its own the kernel sees a thread that is mostly idle
+// and keeps its core's clock low; each frame's burst then runs slowly. Telling
+// the system how long a frame's work took, against the time it has, lets it
+// raise the clock before frames run late. Only in the Android app.
+namespace mhp3rd::android {
+
+// Called once per game frame on the game's thread: `work_ns` is the time the
+// frame kept the thread busy (not the time it slept to hold real time).
+// Opens the session on the first call; does nothing where the system offers
+// no hints or MHP3RD_NO_PERFORMANCE_HINT is set.
+void report_frame_work(std::int64_t work_ns);
+
+} // namespace mhp3rd::android

TeamGDB/PortableKit

Ported from Yakumo 29be2b4ac9.

host/perf/frame_stats.cpp
@@ -3,6 +3,10 @@
 
 #include "settings/settings.hpp"
 
+#if defined(PORTABLEKIT_ANDROID_APP)
+#include "platform/android_performance.hpp"
+#endif
+
 #include <algorithm>
 #include <cstdio>
 #include <cstdlib>
@@ -79,6 +83,7 @@ const StallTrace &stall_trace() {
 struct State {
     // Frame in progress.
     Clock::time_point frame_start{Clock::now()};
+    Clock::time_point frame_start_previous{Clock::now()};  // of the frame end_frame() closes
     Clock::duration render{};
     Clock::duration wait{};
     Clock::duration pacing{};
@@ -324,6 +329,7 @@ void end_frame(std::uint64_t virtual_us, bool presented) {
     if (presented) count_present();
     const Clock::time_point now = Clock::now();
     const double frame_ms = to_ms(now - s.frame_start);
+    s.frame_start_previous = s.frame_start;
     s.frame_start = now;
 
     if (!s.window_has_clock) {
@@ -346,6 +352,11 @@ void end_frame(std::uint64_t virtual_us, bool presented) {
         ++s.gpu_frames;
     }
     ++s.frame_number;
+#if defined(PORTABLEKIT_ANDROID_APP)
+    // The frame's work, without the sleep that holds the game to real time.
+    android::report_frame_work(
+        std::chrono::duration_cast<std::chrono::nanoseconds>(now - (s.frame_start_previous + s.pacing)).count());
+#endif
     const StallTrace &trace = stall_trace();
     if (trace.enabled && frame_ms > trace.slow_frame_ms) {
         // The GPU time is the previous frame's: that is the work a fence wait

host/platform/android_performance.cpp
@@ -1,0 +1,65 @@
+#include "platform/android_performance.hpp"
+
+#include <dlfcn.h>
+#include <unistd.h>
+
+#include <cstdlib>
+#include <iostream>
+
+namespace portablekit::android {
+namespace {
+
+// The time a game frame's work should take: three quarters of the PSP's
+// 29.97 frames a second, so the system raises the clock while a frame still
+// has room rather than once it is late.
+constexpr std::int64_t kTargetWorkNs = 25'000'000;
+
+// Resolved at run time: the functions exist from API 33, and the app runs on
+// Android 11 and later.
+struct Hints {
+    using GetManager = void *(*)();
+    using CreateSession = void *(*)(void *, const std::int32_t *, std::size_t, std::int64_t);
+    using ReportActual = int (*)(void *, std::int64_t);
+    void *session{};
+    ReportActual report{};
+    bool opened{};
+};
+
+Hints &hints() {
+    static Hints value;
+    return value;
+}
+
+void open(Hints &hints) {
+    hints.opened = true;
+    if (portablekit::env("NO_PERFORMANCE_HINT") != nullptr) {
+        std::cout << "[perf] performance hints off (<prefix>_NO_PERFORMANCE_HINT)\n";
+        return;
+    }
+    void *library = dlopen("libandroid.so", RTLD_NOW | RTLD_LOCAL);
+    if (library == nullptr) return;
+    const auto get_manager = reinterpret_cast<Hints::GetManager>(dlsym(library, "APerformanceHint_getManager"));
+    const auto create_session =
+        reinterpret_cast<Hints::CreateSession>(dlsym(library, "APerformanceHint_createSession"));
+    hints.report = reinterpret_cast<Hints::ReportActual>(dlsym(library, "APerformanceHint_reportActualWorkDuration"));
+    if (get_manager == nullptr || create_session == nullptr || hints.report == nullptr) {
+        std::cout << "[perf] no performance hints on this Android version\n";
+        return;
+    }
+    void *manager = get_manager();
+    const std::int32_t thread = static_cast<std::int32_t>(gettid());
+    hints.session = manager != nullptr ? create_session(manager, &thread, 1u, kTargetWorkNs) : nullptr;
+    std::cout << "[perf] performance hints " << (hints.session != nullptr ? "on" : "unavailable")
+              << " for the game's thread, target " << kTargetWorkNs / 1'000'000 << " ms of work a frame\n";
+}
+
+} // namespace
+
+void report_frame_work(std::int64_t work_ns) {
+    Hints &state = hints();
+    if (!state.opened) open(state);
+    if (state.session == nullptr || work_ns <= 0) return;
+    state.report(state.session, work_ns);
+}
+
+} // namespace portablekit::android

host/platform/android_performance.hpp
@@ -1,0 +1,19 @@
+#pragma once
+
+#include <cstdint>
+
+// Android's performance hints (ADPF, APerformanceHint, Android 13 and later).
+// The game's thread works in a burst each frame and then sleeps until real
+// time catches up, so on its own the kernel sees a thread that is mostly idle
+// and keeps its core's clock low; each frame's burst then runs slowly. Telling
+// the system how long a frame's work took, against the time it has, lets it
+// raise the clock before frames run late. Only in the Android app.
+namespace portablekit::android {
+
+// Called once per game frame on the game's thread: `work_ns` is the time the
+// frame kept the thread busy (not the time it slept to hold real time).
+// Opens the session on the first call; does nothing where the system offers
+// no hints or <prefix>_NO_PERFORMANCE_HINT is set.
+void report_frame_work(std::int64_t work_ns);
+
+} // namespace portablekit::android