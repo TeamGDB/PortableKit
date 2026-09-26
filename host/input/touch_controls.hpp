NextOs-Ports/Yakumo

Add on-screen touch controls
For #17 and #103: a pad the host draws over the game on a touch screen,
never into the game's picture. It appears once the screen is touched while
the game runs and hides again when a gamepad, the keyboard or a real mouse
is used.

- The left of the screen is a floating stick: it appears where the thumb
  lands and follows a thumb dragged past its reach. The four face buttons
  sit in the PSP's diamond on the right, sized for thumbs, and a thumb can
  slide from one to the next; L and R are at the top corners, Select, the
  menu button and Start small at the top centre. Every finger is separate,
  so moving, turning and pressing work at once.
- A drag on the free right half turns the camera through camera_input's
  Touch source, Touch camera speed degrees for the screen's height. The
  game camera treats the mouse and touch drags alike, so the analog camera
  and the bow and bowgun aim follow a drag as they follow the mouse.
- The layout (input/touch_controls, no SDL or ImGui) keeps clear of a
  display cutout and is made for 16:9 to 20:9 screens; unit tests cover
  the layout, the fingers and the stick's maths.
- Touches also arrive as mouse events; those never reach the game as a
  mouse, and on Android the pointer is captured only once a real mouse is
  used.
- Controls has a Touch screen section: the controls on or off, opacity,
  size and camera speed (input.touch_* in settings.ini).
- Menus: a finger drag scrolls a list instead of pressing the row it
  started on, and Android's Back opens and closes the menu as Esc does.

Co-Authored-By: Claude Opus 5.5 (1M context) <noreply@anthropic.com>

profiles/mhp3rd/host/input/touch_controls.hpp
@@ -1,0 +1,111 @@
+#pragma once
+
+#include <array>
+#include <cstddef>
+#include <cstdint>
+#include <optional>
+
+// On-screen controls for a touch screen (#17, #103), drawn by the host over
+// the game, never into its picture. This part is the pure logic: where the
+// controls go on a screen of a given shape, which finger holds what, and the
+// pad state and camera turn that come out. The renderer feeds it fingers in
+// window pixels and draws it; nothing here needs SDL or ImGui.
+//
+// The left of the screen is the movement stick, a floating one: it appears
+// where the thumb lands and follows the thumb when it is dragged beyond its
+// reach. The right holds the four face buttons in the PSP's diamond, L and R
+// sit at the top corners, Start, Select and the menu button small at the top
+// centre. A drag anywhere else on the right half turns the camera, as the
+// mouse does. Every finger is independent, so moving, turning and pressing
+// work together.
+namespace mhp3rd::input::touch {
+
+enum class Control : std::uint8_t { Triangle, Circle, Cross, Square, L, R, Start, Select, Menu, Count };
+inline constexpr std::size_t kControls = static_cast<std::size_t>(Control::Count);
+
+// The PSP button (SceCtrlButtons) a control presses; 0 for the menu button.
+[[nodiscard]] std::uint16_t psp_button(Control control);
+
+struct Point {
+    float x{};
+    float y{};
+};
+struct Circle {
+    Point centre;
+    float radius{};
+    [[nodiscard]] bool contains(Point p, float slack = 1.0f) const;
+};
+// Edges the controls keep clear of (a display cutout, rounded corners), px.
+struct Insets {
+    float left{};
+    float top{};
+    float right{};
+    float bottom{};
+};
+
+struct Layout {
+    std::array<Circle, kControls> controls{};
+    float stick_radius{};    // the stick's reach from where the thumb landed
+    float stick_split{};     // x: fingers landing left of it are for the stick
+    float width{};
+    float height{};
+};
+// A layout for a width x height screen. `size` scales every control (1 is the
+// default, sized for thumbs on a phone held sideways).
+[[nodiscard]] Layout make_layout(float width, float height, Insets insets, float size);
+
+// Stick deflection for a thumb at `offset` from the stick's centre, each axis
+// -1..1 (right and down positive), with a dead zone as a fraction of `reach`.
+[[nodiscard]] Point stick_deflection(Point offset, float reach, float dead_zone = 0.12f);
+
+struct Stick {
+    bool active{};
+    Point origin;   // where the stick is centred now
+    Point thumb;    // where the thumb is
+};
+
+class Controls {
+public:
+    void set_layout(const Layout &layout) { layout_ = layout; }
+    [[nodiscard]] const Layout &layout() const { return layout_; }
+
+    void finger_down(std::uint64_t id, Point at);
+    void finger_move(std::uint64_t id, Point at);
+    void finger_up(std::uint64_t id);
+    // Lets go of everything, for when the game loses input (a menu opens).
+    void release_all();
+
+    // PSP buttons held now.
+    [[nodiscard]] std::uint16_t buttons() const;
+    // The stick, each axis -1..1, right and down positive.
+    [[nodiscard]] Point stick() const;
+    [[nodiscard]] const Stick &stick_state() const { return stick_; }
+    // Camera drag since the last take, in pixels.
+    [[nodiscard]] Point take_camera_drag();
+    // The menu button was tapped since the last take.
+    [[nodiscard]] bool take_menu();
+    [[nodiscard]] bool held(Control control) const;
+    [[nodiscard]] bool any_finger() const;
+
+private:
+    enum class Role : std::uint8_t { None, Stick, Control, Camera };
+    struct Finger {
+        std::uint64_t id{};
+        bool used{};
+        Role role{};
+        Control control{};
+        bool over{};    // the finger is still over its control
+        Point last;
+    };
+    static constexpr std::size_t kFingers = 10u;
+    [[nodiscard]] std::optional<Control> control_at(Point at, bool face_only) const;
+    Finger *find(std::uint64_t id);
+
+    Layout layout_{};
+    std::array<Finger, kFingers> fingers_{};
+    Stick stick_{};
+    Point camera_drag_{};
+    bool menu_tapped_{};
+};
+
+} // namespace mhp3rd::input::touch

profiles/mhp3rd/host/ui/touch_overlay.cpp
@@ -1,0 +1,95 @@
+#include "ui/touch_overlay.hpp"
+
+#include <imgui.h>
+
+#include <algorithm>
+#include <cmath>
+
+namespace mhp3rd::ui {
+namespace {
+
+using input::touch::Circle;
+using input::touch::Control;
+
+ImU32 colour(int r, int g, int b, float alpha) {
+    return IM_COL32(r, g, b, static_cast<int>(std::clamp(alpha, 0.0f, 1.0f) * 255.0f));
+}
+
+// The PSP's symbols drawn as shapes, so no font has to carry them.
+void draw_symbol(ImDrawList *list, Control control, ImVec2 c, float r, ImU32 ink, float thickness) {
+    const float s = r * 0.42f;
+    switch (control) {
+    case Control::Triangle:
+        list->AddTriangle({c.x, c.y - s}, {c.x + s * 0.95f, c.y + s * 0.65f}, {c.x - s * 0.95f, c.y + s * 0.65f}, ink,
+                          thickness);
+        break;
+    case Control::Circle: list->AddCircle(c, s * 0.9f, ink, 0, thickness); break;
+    case Control::Cross:
+        list->AddLine({c.x - s * 0.8f, c.y - s * 0.8f}, {c.x + s * 0.8f, c.y + s * 0.8f}, ink, thickness);
+        list->AddLine({c.x + s * 0.8f, c.y - s * 0.8f}, {c.x - s * 0.8f, c.y + s * 0.8f}, ink, thickness);
+        break;
+    case Control::Square:
+        list->AddRect({c.x - s * 0.75f, c.y - s * 0.75f}, {c.x + s * 0.75f, c.y + s * 0.75f}, ink, 0.0f, 0, thickness);
+        break;
+    case Control::Menu:
+        for (int i = -1; i <= 1; ++i)
+            list->AddLine({c.x - s * 0.9f, c.y + i * s * 0.6f}, {c.x + s * 0.9f, c.y + i * s * 0.6f}, ink, thickness);
+        break;
+    default: break;
+    }
+}
+
+const char *label(Control control) {
+    switch (control) {
+    case Control::L: return "L";
+    case Control::R: return "R";
+    case Control::Start: return "START";
+    case Control::Select: return "SELECT";
+    default: return nullptr;
+    }
+}
+
+} // namespace
+
+void draw_touch_controls(const input::touch::Controls &controls, float opacity) {
+    ImDrawList *list = ImGui::GetBackgroundDrawList();
+    const input::touch::Layout &layout = controls.layout();
+    const float base = std::clamp(opacity, 0.1f, 1.0f);
+    // PSP colours for the symbols.
+    const ImU32 symbol_colours[] = {colour(64, 224, 176, base), colour(240, 96, 112, base),
+                                    colour(128, 160, 255, base), colour(232, 144, 208, base)};
+    for (std::size_t i = 0; i < input::touch::kControls; ++i) {
+        const auto control = static_cast<Control>(i);
+        const Circle &circle = layout.controls[i];
+        const ImVec2 c{circle.centre.x, circle.centre.y};
+        const bool held = controls.held(control);
+        const float thickness = std::max(1.5f, circle.radius * 0.08f);
+        list->AddCircleFilled(c, circle.radius, colour(0, 0, 0, base * (held ? 0.55f : 0.3f)));
+        // A dark edge under the light one keeps the outline visible on a bright picture.
+        list->AddCircle(c, circle.radius + thickness, colour(0, 0, 0, base * 0.6f), 0, thickness);
+        list->AddCircle(c, circle.radius, colour(255, 255, 255, base * (held ? 1.0f : 0.8f)), 0, thickness);
+        if (held) list->AddCircleFilled(c, circle.radius * 0.92f, colour(255, 255, 255, base * 0.35f));
+        const ImU32 ink = i < 4u ? symbol_colours[i] : colour(255, 255, 255, base);
+        if (const char *text = label(control)) {
+            ImFont *font = ImGui::GetFont();
+            const float size = circle.radius * (text[1] == '\0' ? 0.9f : 0.42f);
+            const ImVec2 extent = font->CalcTextSizeA(size, 1e9f, 0.0f, text);
+            list->AddText(font, size, {c.x - extent.x * 0.5f, c.y - extent.y * 0.5f}, ink, text);
+        } else {
+            draw_symbol(list, control, c, circle.radius, ink, thickness * 1.3f);
+        }
+    }
+    // The floating stick while the thumb is down: its reach and the knob.
+    const input::touch::Stick &stick = controls.stick_state();
+    if (stick.active) {
+        const ImVec2 origin{stick.origin.x, stick.origin.y};
+        list->AddCircleFilled(origin, layout.stick_radius, colour(0, 0, 0, base * 0.3f));
+        list->AddCircle(origin, layout.stick_radius, colour(255, 255, 255, base * 0.7f), 0,
+                        std::max(1.5f, layout.stick_radius * 0.04f));
+        const input::touch::Point push = controls.stick();
+        const ImVec2 knob{origin.x + push.x * layout.stick_radius, origin.y + push.y * layout.stick_radius};
+        list->AddCircleFilled(knob, layout.stick_radius * 0.45f, colour(255, 255, 255, base * 0.6f));
+    }
+}
+
+} // namespace mhp3rd::ui

profiles/mhp3rd/host/ui/touch_overlay.hpp
@@ -1,0 +1,11 @@
+#pragma once
+
+#include "input/touch_controls.hpp"
+
+namespace mhp3rd::ui {
+
+// Draws the on-screen touch controls over the game in the current ImGui frame,
+// behind any window: outlines at `opacity`, filled while held.
+void draw_touch_controls(const input::touch::Controls &controls, float opacity);
+
+} // namespace mhp3rd::ui

TeamGDB/PortableKit

Ported from Yakumo 33dbcfbd49.

host/input/touch_controls.hpp
@@ -1,0 +1,111 @@
+#pragma once
+
+#include <array>
+#include <cstddef>
+#include <cstdint>
+#include <optional>
+
+// On-screen controls for a touch screen (#17, #103), drawn by the host over
+// the game, never into its picture. This part is the pure logic: where the
+// controls go on a screen of a given shape, which finger holds what, and the
+// pad state and camera turn that come out. The renderer feeds it fingers in
+// window pixels and draws it; nothing here needs SDL or ImGui.
+//
+// The left of the screen is the movement stick, a floating one: it appears
+// where the thumb lands and follows the thumb when it is dragged beyond its
+// reach. The right holds the four face buttons in the PSP's diamond, L and R
+// sit at the top corners, Start, Select and the menu button small at the top
+// centre. A drag anywhere else on the right half turns the camera, as the
+// mouse does. Every finger is independent, so moving, turning and pressing
+// work together.
+namespace portablekit::input::touch {
+
+enum class Control : std::uint8_t { Triangle, Circle, Cross, Square, L, R, Start, Select, Menu, Count };
+inline constexpr std::size_t kControls = static_cast<std::size_t>(Control::Count);
+
+// The PSP button (SceCtrlButtons) a control presses; 0 for the menu button.
+[[nodiscard]] std::uint16_t psp_button(Control control);
+
+struct Point {
+    float x{};
+    float y{};
+};
+struct Circle {
+    Point centre;
+    float radius{};
+    [[nodiscard]] bool contains(Point p, float slack = 1.0f) const;
+};
+// Edges the controls keep clear of (a display cutout, rounded corners), px.
+struct Insets {
+    float left{};
+    float top{};
+    float right{};
+    float bottom{};
+};
+
+struct Layout {
+    std::array<Circle, kControls> controls{};
+    float stick_radius{};    // the stick's reach from where the thumb landed
+    float stick_split{};     // x: fingers landing left of it are for the stick
+    float width{};
+    float height{};
+};
+// A layout for a width x height screen. `size` scales every control (1 is the
+// default, sized for thumbs on a phone held sideways).
+[[nodiscard]] Layout make_layout(float width, float height, Insets insets, float size);
+
+// Stick deflection for a thumb at `offset` from the stick's centre, each axis
+// -1..1 (right and down positive), with a dead zone as a fraction of `reach`.
+[[nodiscard]] Point stick_deflection(Point offset, float reach, float dead_zone = 0.12f);
+
+struct Stick {
+    bool active{};
+    Point origin;   // where the stick is centred now
+    Point thumb;    // where the thumb is
+};
+
+class Controls {
+public:
+    void set_layout(const Layout &layout) { layout_ = layout; }
+    [[nodiscard]] const Layout &layout() const { return layout_; }
+
+    void finger_down(std::uint64_t id, Point at);
+    void finger_move(std::uint64_t id, Point at);
+    void finger_up(std::uint64_t id);
+    // Lets go of everything, for when the game loses input (a menu opens).
+    void release_all();
+
+    // PSP buttons held now.
+    [[nodiscard]] std::uint16_t buttons() const;
+    // The stick, each axis -1..1, right and down positive.
+    [[nodiscard]] Point stick() const;
+    [[nodiscard]] const Stick &stick_state() const { return stick_; }
+    // Camera drag since the last take, in pixels.
+    [[nodiscard]] Point take_camera_drag();
+    // The menu button was tapped since the last take.
+    [[nodiscard]] bool take_menu();
+    [[nodiscard]] bool held(Control control) const;
+    [[nodiscard]] bool any_finger() const;
+
+private:
+    enum class Role : std::uint8_t { None, Stick, Control, Camera };
+    struct Finger {
+        std::uint64_t id{};
+        bool used{};
+        Role role{};
+        Control control{};
+        bool over{};    // the finger is still over its control
+        Point last;
+    };
+    static constexpr std::size_t kFingers = 10u;
+    [[nodiscard]] std::optional<Control> control_at(Point at, bool face_only) const;
+    Finger *find(std::uint64_t id);
+
+    Layout layout_{};
+    std::array<Finger, kFingers> fingers_{};
+    Stick stick_{};
+    Point camera_drag_{};
+    bool menu_tapped_{};
+};
+
+} // namespace portablekit::input::touch

host/ui/touch_overlay.cpp
@@ -1,0 +1,95 @@
+#include "ui/touch_overlay.hpp"
+
+#include <imgui.h>
+
+#include <algorithm>
+#include <cmath>
+
+namespace portablekit::ui {
+namespace {
+
+using input::touch::Circle;
+using input::touch::Control;
+
+ImU32 colour(int r, int g, int b, float alpha) {
+    return IM_COL32(r, g, b, static_cast<int>(std::clamp(alpha, 0.0f, 1.0f) * 255.0f));
+}
+
+// The PSP's symbols drawn as shapes, so no font has to carry them.
+void draw_symbol(ImDrawList *list, Control control, ImVec2 c, float r, ImU32 ink, float thickness) {
+    const float s = r * 0.42f;
+    switch (control) {
+    case Control::Triangle:
+        list->AddTriangle({c.x, c.y - s}, {c.x + s * 0.95f, c.y + s * 0.65f}, {c.x - s * 0.95f, c.y + s * 0.65f}, ink,
+                          thickness);
+        break;
+    case Control::Circle: list->AddCircle(c, s * 0.9f, ink, 0, thickness); break;
+    case Control::Cross:
+        list->AddLine({c.x - s * 0.8f, c.y - s * 0.8f}, {c.x + s * 0.8f, c.y + s * 0.8f}, ink, thickness);
+        list->AddLine({c.x + s * 0.8f, c.y - s * 0.8f}, {c.x - s * 0.8f, c.y + s * 0.8f}, ink, thickness);
+        break;
+    case Control::Square:
+        list->AddRect({c.x - s * 0.75f, c.y - s * 0.75f}, {c.x + s * 0.75f, c.y + s * 0.75f}, ink, 0.0f, 0, thickness);
+        break;
+    case Control::Menu:
+        for (int i = -1; i <= 1; ++i)
+            list->AddLine({c.x - s * 0.9f, c.y + i * s * 0.6f}, {c.x + s * 0.9f, c.y + i * s * 0.6f}, ink, thickness);
+        break;
+    default: break;
+    }
+}
+
+const char *label(Control control) {
+    switch (control) {
+    case Control::L: return "L";
+    case Control::R: return "R";
+    case Control::Start: return "START";
+    case Control::Select: return "SELECT";
+    default: return nullptr;
+    }
+}
+
+} // namespace
+
+void draw_touch_controls(const input::touch::Controls &controls, float opacity) {
+    ImDrawList *list = ImGui::GetBackgroundDrawList();
+    const input::touch::Layout &layout = controls.layout();
+    const float base = std::clamp(opacity, 0.1f, 1.0f);
+    // PSP colours for the symbols.
+    const ImU32 symbol_colours[] = {colour(64, 224, 176, base), colour(240, 96, 112, base),
+                                    colour(128, 160, 255, base), colour(232, 144, 208, base)};
+    for (std::size_t i = 0; i < input::touch::kControls; ++i) {
+        const auto control = static_cast<Control>(i);
+        const Circle &circle = layout.controls[i];
+        const ImVec2 c{circle.centre.x, circle.centre.y};
+        const bool held = controls.held(control);
+        const float thickness = std::max(1.5f, circle.radius * 0.08f);
+        list->AddCircleFilled(c, circle.radius, colour(0, 0, 0, base * (held ? 0.55f : 0.3f)));
+        // A dark edge under the light one keeps the outline visible on a bright picture.
+        list->AddCircle(c, circle.radius + thickness, colour(0, 0, 0, base * 0.6f), 0, thickness);
+        list->AddCircle(c, circle.radius, colour(255, 255, 255, base * (held ? 1.0f : 0.8f)), 0, thickness);
+        if (held) list->AddCircleFilled(c, circle.radius * 0.92f, colour(255, 255, 255, base * 0.35f));
+        const ImU32 ink = i < 4u ? symbol_colours[i] : colour(255, 255, 255, base);
+        if (const char *text = label(control)) {
+            ImFont *font = ImGui::GetFont();
+            const float size = circle.radius * (text[1] == '\0' ? 0.9f : 0.42f);
+            const ImVec2 extent = font->CalcTextSizeA(size, 1e9f, 0.0f, text);
+            list->AddText(font, size, {c.x - extent.x * 0.5f, c.y - extent.y * 0.5f}, ink, text);
+        } else {
+            draw_symbol(list, control, c, circle.radius, ink, thickness * 1.3f);
+        }
+    }
+    // The floating stick while the thumb is down: its reach and the knob.
+    const input::touch::Stick &stick = controls.stick_state();
+    if (stick.active) {
+        const ImVec2 origin{stick.origin.x, stick.origin.y};
+        list->AddCircleFilled(origin, layout.stick_radius, colour(0, 0, 0, base * 0.3f));
+        list->AddCircle(origin, layout.stick_radius, colour(255, 255, 255, base * 0.7f), 0,
+                        std::max(1.5f, layout.stick_radius * 0.04f));
+        const input::touch::Point push = controls.stick();
+        const ImVec2 knob{origin.x + push.x * layout.stick_radius, origin.y + push.y * layout.stick_radius};
+        list->AddCircleFilled(knob, layout.stick_radius * 0.45f, colour(255, 255, 255, base * 0.6f));
+    }
+}
+
+} // namespace portablekit::ui

host/ui/touch_overlay.hpp
@@ -1,0 +1,11 @@
+#pragma once
+
+#include "input/touch_controls.hpp"
+
+namespace portablekit::ui {
+
+// Draws the on-screen touch controls over the game in the current ImGui frame,
+// behind any window: outlines at `opacity`, filled while held.
+void draw_touch_controls(const input::touch::Controls &controls, float opacity);
+
+} // namespace portablekit::ui